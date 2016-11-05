7d22aa8a5f6e0d330bdedc55d35f264ad107bc34
Valentin <valentinsgl@gmail.com>
2016-11-05 17:46:19 +0100

    Adds Spawning batteries

--- Source/BatteryCollector/SpawnVolume.cpp
+++ Source/BatteryCollector/SpawnVolume.cpp
@@ -3,6 +3,7 @@
 #include "BatteryCollector.h"
 #include "SpawnVolume.h"
 #include "Kismet/KismetMathLibrary.h"
+#include "Pickup.h"
 
 
 // Sets default values
@@ -11,8 +12,13 @@ ASpawnVolume::ASpawnVolume()
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
 
-	whereToSpawn = CreateDefaultSubobject<UBoxComponent>(TEXT("whereToSpawn"));
-	RootComponent = whereToSpawn;
+	//initialize a spawn volume
+	WhereToSpawn = CreateDefaultSubobject<UBoxComponent>(TEXT("whereToSpawn"));
+	RootComponent = WhereToSpawn;
+
+	// Initialize default spawn delays
+	SpawnDelayHigh = 4.5f;
+	SpawnDelayLow = 1.0f;
 
 }
 
@@ -21,6 +27,10 @@ void ASpawnVolume::BeginPlay()
 {
 	Super::BeginPlay();
 
+	// Compute the first pickup spawn when volume is spawned
+	SpawnDelay = FMath::RandRange(SpawnDelayLow, SpawnDelayHigh);
+	// We give a handler corresponding to the timer & a callback to spawn the pickup
+	GetWorldTimerManager().SetTimer(SpawnTimer, this, &ASpawnVolume::SpawnPickup, SpawnDelay, false);
 }
 
 // Called every frame
@@ -30,11 +40,49 @@ void ASpawnVolume::Tick(float DeltaTime)
 
 }
 
-FVector ASpawnVolume::getRandomPointInVolume()
+// Will return a random point in the volume
+FVector ASpawnVolume::GetRandomPointInVolume()
 {
-	FVector volumeOrigin = whereToSpawn->Bounds.Origin;
-	FVector volumeExtend = whereToSpawn->Bounds.BoxExtent;
+	// The origin of the box
+	FVector volumeOrigin = WhereToSpawn->Bounds.Origin;
+	// The range of the box
+	FVector volumeExtend = WhereToSpawn->Bounds.BoxExtent;
 
+	// uses a math lib to return the random point
 	return UKismetMathLibrary::RandomPointInBoundingBox(volumeOrigin, volumeExtend);
+}
+
+void ASpawnVolume::SpawnPickup()
+{
+	// Check if we have something to spawn
+	if (WhatToSpawn != NULL) {
+
+		// Check if we have a valid word
+		UWorld * const World = GetWorld();
+		if (World != NULL) {
+
+			// Set the spawn parameters
+			FActorSpawnParameters SpawningParams;
+			SpawningParams.Owner = this;
+			SpawningParams.Instigator = Instigator;
+
+			// We get our random point in volume
+			FVector SpawnLocation = GetRandomPointInVolume();
+
+			// Additionnaly sets a random rotation
+			FRotator SpawnRotation;
+			SpawnRotation.Yaw = FMath::Rand() * 360.0f;
+			SpawnRotation.Pitch = FMath::Rand() * 360.0f;
+			SpawnRotation.Roll = FMath::Rand() * 360.0f;
+
+			// Finally spawn the Pickup
+			APickup* const SpawnedPickup = World->SpawnActor<APickup>(WhatToSpawn, SpawnLocation, SpawnRotation, SpawningParams);
+
+			// We reset a spawn timer to spawn the next pickup
+			SpawnDelay = FMath::RandRange(SpawnDelayLow, SpawnDelayHigh);
+			GetWorldTimerManager().SetTimer(SpawnTimer, this, &ASpawnVolume::SpawnPickup, SpawnDelay, false);
+		
+		}
+	}
 }
 
--- Source/BatteryCollector/SpawnVolume.h
+++ Source/BatteryCollector/SpawnVolume.h
@@ -20,13 +20,40 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaSeconds) override;
 
-	FORCEINLINE class UBoxComponent* getWhereToSpawn() const { return whereToSpawn; }
+	// Provides a spawn box
+	FORCEINLINE class UBoxComponent* GetWhereToSpawn() const { return WhereToSpawn; }
 
-	UFUNCTION(BlueprintPure, Category="Spawing")
-	FVector getRandomPointInVolume();
+	// Provides a random point in spawn box to spawn objects. 
+	// BlueprintPure garantee no side effects thus can be called from blueprint (implies blueprint callable)
+	UFUNCTION(BlueprintPure, Category = "Spawing")
+	FVector GetRandomPointInVolume();
+
+protected:
+
+	// Holds a Pickup to spawn
+	// EditAnywhere means can be modified from property window
+	UPROPERTY(EditAnywhere, Category = "Spawning")
+	TSubclassOf<class APickup> WhatToSpawn;
+
+	// Identify the spawn timer for the object
+	FTimerHandle SpawnTimer;
+
+	// Highest spawn delay
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Spawning")
+	float SpawnDelayHigh;
+
+	// Lowest spawn delay
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Spawning")
+	float SpawnDelayLow;
 
 private:
 
-	UPROPERTY(VisibleAnyWhere, BlueprintReadOnly, Category = "Spawning", meta = (AllowPrivateAccess = "true"))
-	class UBoxComponent* whereToSpawn;	
+	// Variable containing a spawn box
+	// VisibleAnywhere means accessible but not editable from property window, BlueprintReadOnly = cannot be modified by blueprint
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Spawning", meta = (AllowPrivateAccess = "true"))
+	class UBoxComponent* WhereToSpawn;
+
+	void SpawnPickup();
+
+	float SpawnDelay;
 };
